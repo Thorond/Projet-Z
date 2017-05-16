8635d46fbfbf5cc2f6a2233c0bbeec9738423e13
Thorond <arnaud.lefevre@telecom-sudparis.eu>
2017-05-16 15:15:08 +0200

    ajout décors succint donjon salle 1 et salle 4

--- core/src/map/zoneGlace/DonjonSalle4.java
+++ core/src/map/zoneGlace/DonjonSalle4.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.physics.box2d.World;
 import com.mygdx.game.GameMain;
 
 import decors.ClimatMontagneux;
+import map.CadrillageMap;
 import scenes.MainMenu;
 
 /**
@@ -26,6 +27,36 @@ public class DonjonSalle4 extends Sprite {
     public static boolean isCote5Created;
     public static Body cote6 ;
     public static boolean isCote6Created;
+    public static Body cote7 ;
+    public static boolean isCote7Created;
+    public static Body cote8 ;
+    public static boolean isCote8Created;
+
+
+    public static boolean isTonneau1Cut = false;
+    public static boolean isTonneau2Cut = false;
+    public static boolean isTonneau3Cut = false;
+    public static boolean isTonneau4Cut = false;
+    public static boolean isTonneau5Cut = false;
+    public static boolean isTonneau6Cut = false;
+    public static boolean isTonneau7Cut = false;
+
+    public static Body Tonneau1;
+    public static boolean isTonneau1Created;
+    public static Body Tonneau2;
+    public static boolean isTonneau2Created;
+    public static Body Tonneau3;
+    public static boolean isTonneau3Created;
+    public static Body Tonneau4;
+    public static boolean isTonneau4Created;
+    public static Body Tonneau5;
+    public static boolean isTonneau5Created;
+    public static Body Tonneau6;
+    public static boolean isTonneau6Created;
+    public static Body Tonneau7;
+    public static boolean isTonneau7Created;
+
+
 
     public static void sousMap(GameMain game, int x, int y){
 
@@ -173,6 +204,19 @@ public class DonjonSalle4 extends Sprite {
         game.getBatch().draw(ClimatMontagneux.murSombreGlacéCentre, 540+ x, 420+ y);
 
 
+
+        ClimatMontagneux.annimationTorche(game,120+ x,120+ y);
+        ClimatMontagneux.annimationTorche(game,120+ x,180+ y);
+
+
+//
+        ClimatMontagneux.tonneau(isTonneau1Cut ,  game , 480 + x , 360 + y);
+        ClimatMontagneux.tonneau(isTonneau2Cut ,  game , 420 + x , 360 + y);
+        ClimatMontagneux.tonneau(isTonneau3Cut ,  game , 360 + x , 360 + y);
+        ClimatMontagneux.tonneau(isTonneau4Cut ,  game , 300 + x , 300 + y);
+        ClimatMontagneux.tonneau(isTonneau5Cut ,  game , 240 + x , 300 + y);
+        ClimatMontagneux.tonneau(isTonneau6Cut ,  game , 180 + x , 240 + y);
+        ClimatMontagneux.tonneau(isTonneau7Cut ,  game , 480 + x , 60 + y);
     }
 
     public static void destroyBody() {
@@ -194,12 +238,94 @@ public class DonjonSalle4 extends Sprite {
 
         if ( isCote6Created) MainMenu.world.destroyBody(cote6);
         isCote6Created = false;
+
+        if ( isCote7Created) MainMenu.world.destroyBody(cote7);
+        isCote7Created = false;
+
+        if ( isCote8Created) MainMenu.world.destroyBody(cote8);
+        isCote8Created = false;
+
+
+        if ( isTonneau1Created) MainMenu.world.destroyBody(Tonneau1);
+        isTonneau1Created = false;
+
+        if ( isTonneau2Created) MainMenu.world.destroyBody(Tonneau2);
+        isTonneau2Created = false;
+
+        if ( isTonneau3Created) MainMenu.world.destroyBody(Tonneau3);
+        isTonneau3Created = false;
+
+        if ( isTonneau4Created) MainMenu.world.destroyBody(Tonneau4);
+        isTonneau4Created = false;
+
+        if ( isTonneau5Created) MainMenu.world.destroyBody(Tonneau5);
+        isTonneau5Created = false;
+
+        if ( isTonneau6Created) MainMenu.world.destroyBody(Tonneau6);
+        isTonneau6Created = false;
+
+        if ( isTonneau7Created) MainMenu.world.destroyBody(Tonneau7);
+        isTonneau7Created = false;
     }
 
     public static void createBodyAndType(World world) {
         // TODO Auto-generated method stub
+
+
+        if ( CadrillageMap.décorChangé[8][6] == true ) isTonneau1Cut = true;
+        if ( CadrillageMap.décorChangé[7][6] == true ) isTonneau2Cut = true;
+        if ( CadrillageMap.décorChangé[6][6] == true ) isTonneau3Cut = true;
+        if ( CadrillageMap.décorChangé[5][5] == true ) isTonneau4Cut = true;
+        if ( CadrillageMap.décorChangé[4][5] == true ) isTonneau5Cut = true;
+        if ( CadrillageMap.décorChangé[3][4] == true ) isTonneau6Cut = true;
+        if ( CadrillageMap.décorChangé[8][1] == true ) isTonneau7Cut = true;
+
+
+        CadrillageMap.setTypeDeDécor(8,6,"HerbesHautes");
+        CadrillageMap.setTypeDeDécor(7,6,"HerbesHautes");
+        CadrillageMap.setTypeDeDécor(6,6,"HerbesHautes");
+        CadrillageMap.setTypeDeDécor(5,5,"HerbesHautes");
+        CadrillageMap.setTypeDeDécor(4,5,"HerbesHautes");
+        CadrillageMap.setTypeDeDécor(3,4,"HerbesHautes");
+        CadrillageMap.setTypeDeDécor(8,1,"HerbesHautes");
+
+
+        destroyBody();
+
+
+
+        if ( isTonneau1Created == false && ! isTonneau1Cut) {
+            Tonneau1 = ClimatMontagneux.createBodyPerso("tonneau", "static",480,360);
+            isTonneau1Created = true;
+        }
+        if ( isTonneau2Created == false && ! isTonneau2Cut) {
+            Tonneau2 = ClimatMontagneux.createBodyPerso("tonneau", "static",420,360);
+            isTonneau2Created = true;
+        }
+        if ( isTonneau3Created == false && ! isTonneau3Cut) {
+            Tonneau3 = ClimatMontagneux.createBodyPerso("tonneau", "static",360,360);
+            isTonneau3Created = true;
+        }
+        if ( isTonneau4Created == false && ! isTonneau4Cut) {
+            Tonneau4 = ClimatMontagneux.createBodyPerso("tonneau", "static",300,300);
+            isTonneau4Created = true;
+        }
+        if ( isTonneau5Created == false && ! isTonneau5Cut) {
+            Tonneau5 = ClimatMontagneux.createBodyPerso("tonneau", "static",240,300);
+            isTonneau5Created = true;
+        }
+        if ( isTonneau6Created == false && ! isTonneau6Cut) {
+            Tonneau6 = ClimatMontagneux.createBodyPerso("tonneau", "static",180,240);
+            isTonneau6Created = true;
+        }
+        if ( isTonneau7Created == false && ! isTonneau7Cut) {
+            Tonneau7 = ClimatMontagneux.createBodyPerso("tonneau", "static",480,60);
+            isTonneau7Created = true;
+        }
+
+
         if ( isCote1Created == false ) {
-            cote1 = ClimatMontagneux.createBody(80,240,60,480);
+            cote1 = ClimatMontagneux.createBody(140,240,60,480);
             isCote1Created = true;
         }
         if ( isCote2Created == false ) {
@@ -222,10 +348,25 @@ public class DonjonSalle4 extends Sprite {
             cote6 = ClimatMontagneux.createBody(460,440,240,60);
             isCote6Created = true;
         }
+        if ( isCote7Created == false ) {
+            cote7 = ClimatMontagneux.createBody(200,330,60,60);
+            isCote7Created = true;
+        }
+        if ( isCote8Created == false ) {
+            cote8 = ClimatMontagneux.createBody(290,390,120,60);
+            isCote8Created = true;
+        }
     }
 
     public static void destroyType() {
         // TODO Auto-generated method stub
 
+        isTonneau1Cut = false;
+        isTonneau2Cut = false;
+        isTonneau3Cut = false;
+        isTonneau4Cut = false;
+        isTonneau5Cut = false;
+        isTonneau6Cut = false;
+        isTonneau7Cut = false;
     }
 }
